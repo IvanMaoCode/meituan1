dde3b8badb52f04cb61ba1ec0bc5583a57ccc610
XMG <kluthmao@163.com>
2019-08-14 23:41:47 +0800

    创建了Person类

---
+++ weibo/weibo/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  weibo
+//
+//  Created by Kluth on 2019/8/14.
+//  Copyright © 2019 Kluth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ weibo/weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weibo
+//
+//  Created by Kluth on 2019/8/14.
+//  Copyright © 2019 Kluth. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
