44e7b1cfe6123816bc51083a230d0da3f976048d
XMG <kluthmao@163.com>
2019-08-14 23:32:30 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by Kluth on 2019/8/14.
+//  Copyright © 2019 Kluth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  weibo
+//
+//  Created by Kluth on 2019/8/14.
+//  Copyright © 2019 Kluth. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
